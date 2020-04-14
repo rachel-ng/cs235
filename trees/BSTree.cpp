11ebe5ad36c0889e00a5b5d9c82eedc66e03441c
rachel-ng <rachel.ng.135@gmail.com>
2020-04-13 22:08:17 -0400

    .

--- trees/BSTree.cpp
+++ trees/BSTree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "Node.h"
 #include "BSTree.h"
 
@@ -9,6 +10,7 @@ BSTree::BSTree(){
 void BSTree::insert(int d){
   
 }
+
 std::string BSTree::get_debug_string(){
 
   if (root == nullptr){
@@ -16,7 +18,8 @@ std::string BSTree::get_debug_string(){
   } else {
     return  std::to_string( root->getData()) + std::to_string(root->getRight()->getData());
   }
-};
+}
+
 void BSTree::setup(){
   Node *n = new Node(10);
   root = n;
@@ -27,4 +30,29 @@ void BSTree::setup(){
   n = new Node(40);
   root->getLeft()->setLeft(n);
 }
-  
+
+// VIDEO 5
+std::string BSTree::get_debug_string_r (){
+    return debug_string_r(root, 0);
+}
+
+std::string BSTree::debug_string_r (Node *n, int l) {
+    std::string s;
+    
+    for (int i = 0; i < l; i++) {
+        s += "\t";
+    }
+    
+    s += "|-- ";
+
+    if (n == nullptr) {
+        s += "null";
+    }
+    else {
+        s += std::to_string(n->getData());
+        s += "\n" + debug_string_r(n->getLeft(), l+1); 
+        s += "\n" + debug_string_r(n->getRight(), l+1); 
+    }
+    return s; 
+}
+
--- trees/BSTree.h
+++ trees/BSTree.h
@@ -1,15 +1,16 @@
 #pragma once
 #include <stdio.h>
+#include <vector>
 
 class BSTree{
  private:
   Node *root;
+  std::string debug_string_r (Node *n, int l);
 
  public:
-  BSTree();
-  void insert(int d);
-  std::string get_debug_string();
-  void setup();
-  
-  
+  BSTree ();
+  void insert (int d);
+  std::string get_debug_string ();
+  std::string get_debug_string_r ();
+  void setup ();
 };
